8a6cfeca1fcd16bc80301470d8570ba5f6174948
Fichberg <rfichberg@gmail.com>
2017-06-05 22:10:59 -0300

    Image channels, fx and bijection

--- src/argument_checker.m
+++ src/argument_checker.m
@@ -4,63 +4,13 @@
 1;
 
 # Extract values from CLI
-function [nx, ny, ax, ay, bx, by, tests_enable, img] = extract(args)
+function [tests_enable, img] = extract(args)
   tests_enable = false;
   img = "";
 
-  if length(args) < 12
-    printf("Missing arguments. Read documentation for further information.");
-    exit;
-  elseif length(args) > 15
-    printf("Too many arguments. Read documentation for further information.");
-    exit;
-  endif
-
   i = 1;
   while i <= length(args)
-    if strcmp("-nx", args{i}) && (i < length(args))
-      nx = floor(str2num(args{i + 1}));
-      if nx < str2num(args{i + 1})
-        printf("WARNING: detected %s as argument for '-nx' parameter. Will be used %d instead (must be integer).\n", args{i + 1}, by);
-      endif
-      if isempty(nx)
-        printf("Expected an integer as argument for '-nx' parameter.");
-        exit;
-      endif
-    elseif strcmp("-ny", args{i}) && (i < length(args))
-      ny = floor(str2num(args{i + 1}));
-      if ny < str2num(args{i + 1})
-        printf("WARNING: detected %s as argument for '-ny' parameter. Will be used %d instead (must be integer).\n", args{i + 1}, by);
-      endif
-      if isempty(ny)
-        printf("Expected an integer as argument for '-ny' parameter.");
-        exit;
-      endif
-    elseif strcmp("-ax", args{i}) && (i < length(args))
-      ax = str2double(args{i + 1});
-      if isnan(ax)
-        printf("Expected a double as argument for '-ax' parameter.");
-        exit;
-      endif
-    elseif strcmp("-ay", args{i}) && (i < length(args))
-      ay = str2double(args{i + 1});
-      if isnan(ay)
-        printf("Expected a double as argument for '-ay' parameter.");
-        exit;
-      endif
-    elseif strcmp("-bx", args{i}) && (i < length(args))
-      bx = str2double(args{i + 1});
-      if isnan(bx)
-        printf("Expected a double as argument for '-bx' parameter.");
-        exit;
-      endif
-    elseif strcmp("-by", args{i}) && (i < length(args))
-      by = str2double(args{i + 1});
-      if isnan(by)
-        printf("Expected a double as argument for '-by' parameter.");
-        exit;
-      endif
-    elseif strcmp("--image", args{i}) && (i < length(args))
+    if strcmp("--image", args{i}) && (i < length(args))
       if exist(strcat("../images/", args{i + 1}), "file") == 2
         img = strcat("../images/", args{i + 1});
       else
@@ -77,24 +27,4 @@ function [nx, ny, ax, ay, bx, by, tests_enable, img] = extract(args)
     endif
     i += 2;
   endwhile
-
-  if ax >= bx
-    printf("Expected ax < bx.");
-    exit;
-  endif
-
-  if ay >= by
-    printf("Expected ay < by.");
-    exit;
-  endif
-
-  if nx <= 0
-    printf("Expected positive nx.");
-    exit;
-  endif
-
-  if ny <= 0
-    printf("Expected positive ny.");
-    exit;
-  endif
 endfunction
--- src/bivariate_interpolation.m
+++ src/bivariate_interpolation.m
@@ -5,8 +5,6 @@ argument_checker;
 
 function main(args)
   ##########
-  # Select a function f
-  f = @f1;
   # Select mode. 0 = bilinear. 1 = bicubic.
   mode = 1;
   # Select x value. x must be in [ax, bx]
@@ -17,19 +15,72 @@ function main(args)
   compression_rate = 5;
   ##########
 
-  [nx, ny, ax, ay, bx, by, tests_enable, img] = extract(args);
+  [tests_enable, image_path] = extract(args);
+  [image_, image_R, image_G, image_B] = get_image_matrices(image_path);
+  [nx, ny, ax, ay, bx, by, hx, hy] = get_image_parameters(image_);
+  printf("Computing bijection.\n");
+  [fx, bijection] = do_bijection(image_R);
+  
+
+
+
 endfunction
 
+# Bijection bewtween pixels values andassociated integers
+function [fx, bijection] = do_bijection(image_)
+  fx = [];
+  bijection = [];
+  associated_integer = 0;
 
+  row = rows(image_);
+  while row >= 1
+    column = 1;
+    fx_row = [];
 
+    while column <= columns(image_)
+      integer_index = 1;
 
+      while integer_index <= rows(bijection)
+        if bijection(integer_index, 2) == image_(row, column)
+          break;
+        endif
+        integer_index++;
+      endwhile
 
+      if integer_index > rows(bijection)
+        bijection = [bijection; [associated_integer++, image_(row, column)]];
+      endif
+      fx_row = [fx_row, bijection(integer_index, 1)];
 
-# Test functions
-##############################################################
-function z = f1(x, y)
-  z = x^2 + y^2;
+      column++;
+      #row
+      #column
+    endwhile
+    fx = [fx_row; fx];
+    row--;
+  endwhile
 endfunction
+
+# Get image parameters
+function [nx, ny, ax, ay, bx, by, hx, hy] = get_image_parameters(image_)
+  ax = 0;
+  ay = 0;
+  bx = columns(image_);
+  by = rows(image_);
+  nx = bx - ax;
+  ny = by - ay;
+  hx = (bx - ax) / nx; # = 1 for the image
+  hy = (by - ay) / ny; # = 1 for the image
+endfunction
+
+# Get image and its RGB channels matircs
+function [image_, image_R, image_G, image_B] = get_image_matrices(image_path)
+  image_ = imread(image_path);
+  image_R = image_(:,:,1);
+  image_G = image_(:,:,2);
+  image_B = image_(:,:,3);
+endfunction
+
 
 
 main(argv());
